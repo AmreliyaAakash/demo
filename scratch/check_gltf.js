d6f9cd0052499cdd37732188b5569cff829c236e
AmreliyaAakash <amreliyaaakash3@gmail.com>
2026-04-16 16:32:43 +0530

    Deployment fix: Implement dynamic mesh mapping in Cup.jsx to resolve runtime geometry error

---
+++ scratch/check_gltf.js
@@ -0,0 +1,5 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import fs from 'fs';
+
+// This is just for my mental model, I'll use a better way.
+console.log("Checking GLTF...");
